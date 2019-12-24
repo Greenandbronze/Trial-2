f81ae59c6afd5ffd946e978a4cb5df773cfc6e5a
Greenandbronze <59208582+Greenandbronze@users.noreply.github.com>
2019-12-24 14:18:11 -0500

    Add files via upload

---
+++ Market Cap Automation.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from urllib.request import urlopen as uo
+from bs4 import BeautifulSoup as bs
+
+html = uo('https://finance.yahoo.com/quote/NEM?p=NEM&.tsrc=fin-srch')
+read= bs(html.read(),'html.parser')
+MarketCap= read.find('td', {'data-test':'MARKET_CAP-value'})
+
+
+print(MarketCap.get_text())
+
