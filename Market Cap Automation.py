4a406fa016f7e022dd75e4e43d1c0e30515b3735
Greenandbronze <59208582+Greenandbronze@users.noreply.github.com>
2019-12-24 16:00:14 -0500

    Update Market Cap Automation.py

--- Market Cap Automation.py
+++ Market Cap Automation.py
@@ -50,3 +50,42 @@ MarketCap= read.find('td', {'class':'Fz(s) Fw(500) Ta(end) Pstart(10px) Miw(60px
 print(MarketCap.get_text())
 #Prints Top 5 Dataframe results and Market Cap from Yahoo Finance.
 print(Tickers.head())
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    'Ticker_Symbol' is not defined Error
+    import pandas as pd
+import xlrd as xl
+
+Workbook=xl.open_workbook('Ticker_List.xlsx')
+Worksheet= Workbook.sheet_by_index(0)
+Total_Rows=Worksheet.nrows
+Start_Row= 1
+
+
+def loop_through_excel (Start_Row, Total_Rows):
+    for row_cursor in range (Start_Row,Total_Rows):
+        Ticker_Symbol= Worksheet.cell(row_cursor,0).value
+        print (Ticker_Symbol)
+        
+Ticker_List= 'Ticker_List.xlsx'
+Tickers= pd.read_excel(Ticker_List,
+                       header=0,
+                       index_col=False,
+                       keep_default_na=True)
+
+
+for i in Ticker_Symbol:
+       html = 'https://finance.yahoo.com/quote/'+ i + '/key-statistics?p=' + i + "'"
+     
+       print(html)
